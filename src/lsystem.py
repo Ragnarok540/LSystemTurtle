064cf5727e1499e7289f62b82ca496de2bcf9fff
Edgar Nova <ragnarok540@gmail.com>
2019-03-03 20:43:25 -0500

    command line

--- src/lsystem.py
+++ src/lsystem.py
@@ -28,39 +28,5 @@ class LSystem:
         }
         for char in list(ls):
             func = switcher.get(char, lambda: None)
-            func()        
+            func()
 
-lsystem = LSystem('A', {'A': 'AB', 'B': 'A'})
-
-print(lsystem.generation(0, lsystem.axiom))
-print(lsystem.generation(1, lsystem.axiom))
-print(lsystem.generation(2, lsystem.axiom))
-print(lsystem.generation(3, lsystem.axiom))
-
-lsystem = LSystem('0', {'1': '11', '0': '1[0]0'})
-
-print(lsystem.generation(0, lsystem.axiom))
-print(lsystem.generation(1, lsystem.axiom))
-print(lsystem.generation(2, lsystem.axiom))
-print(lsystem.generation(3, lsystem.axiom))
-
-lsystem = LSystem('F', {'F': 'F+F-F-F+F'})
-
-print(lsystem.generation(0, lsystem.axiom))
-print(lsystem.generation(1, lsystem.axiom))
-print(lsystem.generation(2, lsystem.axiom))
-print(lsystem.generation(3, lsystem.axiom))
-
-#Sierpinski A
-lsystem = LSystem('F-G-G', {'F': 'F-lG+F+G-F', 'G': 'GG'})
-
-#Sierpinski B
-lsystem = LSystem('F', {'F': 'G-F-G', 'G': 'F+G+F'})
-
-#Dragon Curve
-lsystem = LSystem('FX', {'X': 'X+YF+', 'Y': '-FX-Y'})
-
-#Twindragon
-lsystem = LSystem('FX+FX+', {'X': 'X+YF', 'Y': 'FX-Y'})
-
-lsystem.draw(10, 5, 90)
---
+++ src/main.py
@@ -0,0 +1,48 @@
+import cmd, sys
+from lsystem import LSystem
+
+class LSystemShell(cmd.Cmd):
+    intro = 'Welcome to the LSystem shell. Type help or ? to list commands.\n'
+    prompt = '(lsystem) '
+
+    def preloop(self):
+        self.lsystem = None
+        super(LSystemShell, self).preloop()
+    
+    def do_set(self, arg):
+        'Set LSystem (algae, drangoncurve, twindragon)'
+        switcher = {
+            'algae': LSystem('A', {'A': 'AB', 'B': 'A'}),
+            'dragoncurve': LSystem('FX', {'X': 'X+YF+', 'Y': '-FX-Y'}),
+            'twindragon': LSystem('FX+FX+', {'X': 'X+YF', 'Y': 'FX-Y'})
+        }
+        self.lsystem = switcher.get(arg, None)
+
+    def do_draw(self, arg):
+        'Draw the LSystem (args: generation distance angle)'
+        if self.lsystem == None:
+            print('LSystem must be set first')
+        else:
+            self.lsystem.draw(*parse(arg))
+
+    def do_print(self, arg):
+        'Print the LSystem string (arg: generation)'
+        if self.lsystem == None:
+            print('LSystem must be set first')
+        else:
+            print(self.lsystem.generation(int(arg), self.lsystem.axiom))
+
+    def do_def(self, arg):
+        'Print definition of the LSystem'
+        if self.lsystem == None:
+            print('LSystem must be set first')
+        else:
+            print(f'Axiom: {self.lsystem.axiom}')
+            print(f'Rules: {self.lsystem.rules}')
+
+def parse(arg):
+    'Convert a series of zero or more numbers to an argument tuple'
+    return tuple(map(int, arg.split()))
+
+if __name__ == '__main__':
+    LSystemShell().cmdloop()
